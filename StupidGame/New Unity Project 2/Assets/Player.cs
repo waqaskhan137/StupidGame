7d2a05e08346398e9a98443b5910e1339281a1e8
Danish Jalil <danish@tintash.com>
2014-09-15 23:10:10 +0500

    Initial Commit

---
+++ StupidGame/New Unity Project 2/Assets/Player.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 10f;
+	public Vector2 maxVelocity = new Vector2(3, 5);
+	
+	// Update is called once per frame
+	void Update () {
+		var forceX = 0f;
+		var forceY = 0f;
+
+		//if (Input.touchCount > 0) {
+
+		
+
+		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
+
+		if (Input.GetKey ("right")) {
+
+			if(absVelX < maxVelocity.x)
+				forceX = speed;
+
+			transform.localScale = new Vector3(1, 1, 1);
+
+		} else if (Input.GetKey ("left")) {
+
+			if(absVelX < maxVelocity.x)
+				forceX = -speed;
+
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+
+		if (Input.GetButtonDown("KeyCode.Space")) {
+			if(transform.position.y <0){
+			forceY = 30;
+			transform.parent = null;
+			}
+			else {
+				forceY = 0;
+				transform.parent = null;
+
+			}
+		
+		}
+
+		rigidbody2D.AddForce (new Vector2 (forceX, forceY));
+
+		
+		if ((transform.position.y) > 1) {
+			transform.position = new Vector2(transform.position.x , 1);	
+		}
+	
+	}
+}
+//}
